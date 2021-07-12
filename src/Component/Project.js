518b76ecd9d23fd39c12cc39eef6b3296b0f5ecc
Peranut Boonyakit <peranut.pepo@hotmail.com>
2021-07-12 13:23:03 +0700

    project

--- src/Component/Project.js
+++ src/Component/Project.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Image } from "@chakra-ui/image";
+import exchange from '../Image/exchange.png'
+import './Project.css'
 
 const Skill = () => {
   return (
@@ -6,6 +9,12 @@ const Skill = () => {
       <h1 className="h1">Project</h1>
 
       <div className="icon-skill">
+        <a href='https://peranutboonyakit.github.io/ProjectJS/Exchange%20Money/index.html'>
+          <Image src={exchange} borderRadius='10px' boxSize='170px' m={5} /><p>Exchange Money</p></a>
+        <a href='https://peranutboonyakit.github.io/ProjectJS/Exchange%20Money/index.html'>
+          <Image src={exchange} borderRadius='10px' boxSize='170px' m={5} /><p>Exchange Money</p></a>
+        <a href='https://peranutboonyakit.github.io/ProjectJS/Exchange%20Money/index.html'>
+          <Image src={exchange} borderRadius='10px' boxSize='170px' m={5} /><p>Exchange Money</p></a>
       </div>
     </div>
   );
